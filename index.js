991bbea99793fbecca2531e38d0cebfd7854c8be
AspiringSaint <wilberttosoydev@outlook.com>
2025-07-16 16:02:28 +0800

    added general error handler

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { errorHandler } = require('./middlewares/error');
+
 // Middlewares
 app.use(express.json());
 
+
+// Not found handler
+app.use((req, res, next) => {
+    res.status(404);
+    const error = new Error(`Not Found: ${req.originalUrl}`);
+    next(error);
+});
+
+
+// Global error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Asset Tracker API is running on http://localhost:${port}`);
