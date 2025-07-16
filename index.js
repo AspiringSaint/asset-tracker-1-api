40a6b1ea58bc7703c6bb2825b7da84567a988a71
AspiringSaint <wilberttosoydev@outlook.com>
2025-07-16 12:28:13 +0800

    added the express.json middleware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middlewares
+app.use(express.json());
+
 
 app.listen(port, () => {
   console.log(`Asset Tracker API is running on http://localhost:${port}`);
