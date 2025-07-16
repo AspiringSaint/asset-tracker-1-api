414115368bd18e7067c90a6fcccb4e3368893675
AspiringSaint <wilberttosoydev@outlook.com>
2025-07-16 17:12:43 +0800

    added the mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,18 +10,23 @@ const { errorHandler } = require('./middlewares/error');
 // Middlewares
 app.use(express.json());
 
-
 // Not found handler
 app.use((req, res, next) => {
-    res.status(404);
-    const error = new Error(`Not Found: ${req.originalUrl}`);
-    next(error);
+  res.status(404);
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  next(error);
 });
-
 
 // Global error handler
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Asset Tracker API is running on http://localhost:${port}`);
-});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(port, () => {
+    console.log(`Asset Tracker API is running on http://localhost:${port}`);
+  });
+})
+.catch(err => console.error(`MongoDB connection error: ${err.message}`));
